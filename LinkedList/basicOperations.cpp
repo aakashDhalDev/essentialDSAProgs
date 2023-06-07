88fc4e507a5b084ca35654e1d87d630dfb62915f
Aakash Dhal <aakash.dhal.rru@gmail.com>
2023-06-07 15:33:06 +0530

    added basic linked list opeartions using STL

--- LinkedList/basicOperations.cpp
+++ LinkedList/basicOperations.cpp
@@ -3,21 +3,71 @@
 
 using namespace std;
 
-int main()
+void print(list<int> numbers)
 {
-
-    // create the list
-    list<int> numbers{1, 2, 3, 4};
-
-    numbers.push_front(0);
-    numbers.push_back(5);
-
     // display the elements of the list
     cout << "List Elements: ";
     for (int number : numbers)
     {
         cout << number << "->";
     }
+    cout << endl;
+}
 
+int main()
+{
+
+    // create the list
+    list<int> numbers{1, 2, 3, 4};
+
+    print(numbers);
+
+    // adds new elemrnt to the beginning of list
+    numbers.push_front(0);
+    cout << "adds new element to the beginning of list" << endl;
+    print(numbers);
+    cout << endl;
+
+    // adds element to the end of list
+    numbers.push_back(5);
+    cout << "adds element to the end of list" << endl;
+    print(numbers);
+    cout << endl;
+
+    // remove first element of the  list
+    numbers.pop_front();
+    cout << "remove first element of the  list" << endl;
+    print(numbers);
+    cout << endl;
+
+    // remove last element of the  list
+    numbers.pop_back();
+    cout << "remove last element of the  list" << endl;
+    print(numbers);
+    cout << endl;
+
+    // insert element at a specified position in a list
+    auto it = numbers.begin();
+    advance(it, 2);
+    numbers.insert(it, 2, 7);
+    cout << "insert element at a specified position in a list" << endl;
+    print(numbers);
+    cout << endl;
+
+    // remove all nodes that have a specific value
+    numbers.remove(7);
+    cout << "remove all nodes that have a specific value" << endl;
+    print(numbers);
+    cout << endl;
+
+    // delete nodes in a range
+    auto start = numbers.begin();
+    advance(start, 1);
+    auto end = numbers.begin();
+    advance(end, 3);
+    numbers.erase(start, end);
+    cout << "delete nodes in a range" << endl;
+    print(numbers);
+    cout << endl;
     return 0;
 }
